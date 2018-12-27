31e91cb3f5e645b4e1bdbdfd88607551d79910e8
robbyxp1 <robxp@hotmail.com>
2018-12-27 15:42:56 +0000

    Moved to better program start and added embroyonic conditionUC

---
+++ TestExtendedControls/TestConditionUC.cs
@@ -0,0 +1,22 @@
+﻿using ExtendedControls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DialogTest
+{
+    public partial class TestConditionUC: Form
+    {
+        public TestConditionUC()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
