d2aeafb0ec24fddd0e18244009b7e46318b2fdcd
Andrea Spada <spada.andrea@gmail.com>
2019-02-06 18:24:57 -0200

    Add a Material Dark theme

--- ExtendedControls/Themes/ThemeStandard.cs
+++ ExtendedControls/Themes/ThemeStandard.cs
@@ -269,6 +269,23 @@ namespace ExtendedControls
             if (IsFontAvailable("Calisto MT"))
                 themelist.Add(new Settings(themelist[themelist.Count - 1], "Elite Calisto", "Calisto MT", 12F));
 
+            themelist.Add(new Settings("Material Dark", Color.FromArgb(255, 54, 57, 63), // form
+                Color.FromArgb(255, 75, 75, 75), Color.FromArgb(255, 255, 160, 0), Color.FromArgb(255, 41, 46, 51), buttonstyle_flat, // button back, text, border
+                Color.FromArgb(255, 47, 49, 54), Color.FromArgb(255, 255, 160, 0), // grid borderback, bordertext
+                Color.FromArgb(255, 37, 37, 38), Color.FromArgb(255, 223, 227, 238), Color.FromArgb(255, 82, 94, 164), // grid cellbackground, text, borderlines
+                Color.FromArgb(255, 37, 37, 38), Color.FromArgb(255, 82, 94, 164), Color.FromArgb(255, 82, 94, 164), // grid sliderback, arrow, scrollbutton
+                Color.FromArgb(255, 192, 192, 192), Color.FromArgb(255, 202, 202, 255), // travelgrid_nonvisited, visited
+                Color.FromArgb(255, 47, 49, 54), Color.FromArgb(255, 240, 240, 240), Color.FromArgb(255, 255, 160, 0), Color.FromArgb(255, 90, 196, 90), Color.FromArgb(255, 82, 94, 164), textboxborderstyle_color, // textbox back, fore, highlight, success, border
+                Color.FromArgb(255, 47, 49, 54), Color.FromArgb(255, 255, 160, 0), Color.FromArgb(255, 75, 75, 75), // text sliderback, scrollarrow, scrollbutton
+                Color.FromArgb(255, 240, 240, 240), Color.FromArgb(255, 255, 160, 0), // checkbox, checkboxtick
+                Color.FromArgb(255, 60, 55, 50), Color.FromArgb(255, 196, 196, 196), Color.FromArgb(255, 60, 55, 50), Color.FromArgb(255, 245, 245, 245),  // menuback, fore, dropdownback, dropdownfore
+                Color.FromArgb(255, 240, 240, 240),  // label
+                Color.FromArgb(255, 45, 50, 55), Color.FromArgb(255, 255, 160, 0), Color.FromArgb(255, 129, 138, 192), // group back, text, border
+                Color.FromArgb(255, 40, 45, 50), // tab control borderlines
+                Color.FromArgb(255, 75, 75, 75), Color.FromArgb(255, 45, 50, 55), Color.FromArgb(255, 40, 45, 50), // toolstrip, back, border
+                Color.FromArgb(255, 250, 150, 8), // spanel
+                false, 100, "Microsoft Sans Serif", 9.75F));
+
             themelist.Add(new Settings("Easy Dark", Color.FromArgb(255, 65, 65, 65), // form
                 Color.FromArgb(255, 75, 75, 75), Color.FromArgb(255, 245, 120, 30), Color.FromArgb(255, 41, 46, 51), buttonstyle_flat, // button back, text, border
                 Color.FromArgb(255, 62, 68, 77), Color.FromArgb(255, 255, 120, 30), // grid borderback, bordertext
