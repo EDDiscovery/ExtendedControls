50a280b110d19038dbe0caa08a9f0d8b9ecc951d
robbyxp1 <robxp@hotmail.com>
2021-05-24 11:25:26 +0100

    Add more tests for DGV

--- TestExtendedControls/TestPanelDGV.cs
+++ TestExtendedControls/TestPanelDGV.cs
@@ -24,79 +24,43 @@ namespace TestExtendedControls
             theme.SetThemeByName("Elite Verdana");
             theme.WindowsFrame = true;
 
+            dataGridView1.Dock = DockStyle.Fill;
+
             for ( int i = 0; i < 100; i++ )
             {
                 DataGridViewRow row = dataGridView1.RowTemplate.Clone() as DataGridViewRow;
-                row.CreateCells(dataGridView1, i.ToString(), "2", "3");
+                row.CreateCells(dataGridView1, i.ToString(), (100+i).ToString(), (10000-i).ToString());
+                row.Tag = i;
                 dataGridView1.Rows.Add(row);
             }
+        }
 
-            System.Diagnostics.Debug.WriteLine("HW {0} ", dataGridView1.RowHeadersWidth);
-            for (int i = 0; i < dataGridView1.Columns.Count; i++)
-                System.Diagnostics.Debug.WriteLine("Col {0} w {1} ", i, dataGridView1.Columns[i].Width);
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            Location = new Point(100, 10);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            int[] prevsizes = new int[] { 100, 200, 300 };
-
-            int totalsizes = 0;
-            foreach (var x in prevsizes)
-                totalsizes += x;
-
-            dataGridView1.SuspendLayout();
-            dataGridView1.RowHeadersWidth = 50;
-
-            for (int i = 0; i < prevsizes.Length; i++)
-            {
-                float f = 100f * (float)prevsizes[i] / totalsizes;
-                dataGridView1.Columns[i].FillWeight = f;
-            }
-            dataGridView1.ResumeLayout();
-
-            System.Diagnostics.Debug.WriteLine("HW {0} ", dataGridView1.RowHeadersWidth);
-            for (int i = 0; i < dataGridView1.Columns.Count; i++)
-                System.Diagnostics.Debug.WriteLine("Col {0} w {1} ", i, dataGridView1.Columns[i].Width);
+            for (int r = 5; r < 10; r++)
+                dataGridView1.Rows[r].Cells[0].Value= "";       // col 0 sort by tag, knock some out and show it still sorts right
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            dataGridView1.SuspendLayout();
-            dataGridView1.RowHeadersWidth = 75;
-            dataGridView1.Columns[0].Width = 200;
-            dataGridView1.Columns[1].Width = 400;
-            dataGridView1.Columns[2].Width = dataGridView1.Width - dataGridView1.RowHeadersWidth - 200 - 400;
-
-            System.Diagnostics.Debug.WriteLine("HW {0} ", dataGridView1.RowHeadersWidth);
-            for (int i = 0; i < dataGridView1.Columns.Count; i++)
-                System.Diagnostics.Debug.WriteLine("Col {0} w {1} ", i, dataGridView1.Columns[i].Width);
-
-            dataGridView1.ResumeLayout();
         }
 
         private void Button3_Click(object sender, EventArgs e)
         {
-            RescaleColumns(dataGridView1);
         }
 
         private void Button4_Click(object sender, EventArgs e)
         {
-            System.Diagnostics.Debug.WriteLine("HW {0} ", dataGridView1.RowHeadersWidth);
-            int wset = dataGridView1.RowHeadersWidth;
-            for (int i = 0; i < dataGridView1.Columns.Count; i++)
-                wset += dataGridView1.Columns[i].Width;
-
-            for (int i = 0; i < dataGridView1.Columns.Count; i++)
-            {
-                System.Diagnostics.Debug.WriteLine("Col {0} w {1} {2}", i, dataGridView1.Columns[i].Width, (float)dataGridView1.Columns[i].Width/wset);
-            }
-
-            System.Diagnostics.Debug.WriteLine("{0} vs {1}", dataGridView1.Width, wset);
         }
 
         private void dataGridView1_Resize(object sender, EventArgs e)
         {
-           // RescaleColumns2(dataGridView1);
         }
 
         private void dataGridView1_ColumnWidthChanged(object sender, DataGridViewColumnEventArgs e)
@@ -114,50 +78,17 @@ namespace TestExtendedControls
 
         }
 
-        // experimental functions ..
-
-        private void RescaleColumns(DataGridView v)
+        private void dataGridView1_SortCompare(object sender, DataGridViewSortCompareEventArgs e)
         {
-            int colwidth = v.RowHeadersWidth;
-            for (int i = 0; i < v.ColumnCount; i++)
-                colwidth += v.Columns[i].Width;
-            int curwidth = v.Width;
-            System.Diagnostics.Debug.WriteLine("{0} vs {1}", colwidth, curwidth);
-            float scalingfactor = (float)curwidth / (float)colwidth;
-
-            v.RowHeadersWidth = Math.Max(10, (int)(v.RowHeadersWidth * scalingfactor));
-            int wset = v.RowHeadersWidth;
-
-            for (int i = 0; i < v.ColumnCount - 1; i++)
+            if (e.Column.Index == 0)
             {
-                v.Columns[i].Width = (int)(v.Columns[i].Width * scalingfactor);
-                wset += v.Columns[i].Width;
+                int tl = (int)dataGridView1.Rows[e.RowIndex1].Tag;
+                int tr = (int)dataGridView1.Rows[e.RowIndex2].Tag;
+                e.SortResult = tl.CompareTo(tr);
+                e.Handled = true;
             }
-
-            v.Columns[v.ColumnCount - 1].Width = curwidth - wset;
-
-            System.Diagnostics.Debug.WriteLine("HW {0} ", dataGridView1.RowHeadersWidth);
-            for (int i = 0; i < dataGridView1.Columns.Count; i++)
-                System.Diagnostics.Debug.WriteLine("Col {0} w {1} ", i, dataGridView1.Columns[i].Width);
+            else
+                e.SortDataGridViewColumnNumeric();
         }
-
-        private void RescaleColumns2(DataGridView v)
-        {
-            int widthleft = v.Width;
-
-            for (int i = 0; i < v.ColumnCount; i++)
-            {
-                v.Columns[i].Width = (int)(v.Width * (float)v.Columns[i].FillWeight / 100.0f);
-                widthleft -= v.Columns[i].Width;
-            }
-
-            v.RowHeadersWidth = Math.Max(widthleft, 10);
-
-            System.Diagnostics.Debug.WriteLine("HW {0} ", dataGridView1.RowHeadersWidth);
-            for (int i = 0; i < dataGridView1.Columns.Count; i++)
-                System.Diagnostics.Debug.WriteLine("Col {0} w {1} ", i, dataGridView1.Columns[i].Width);
-        }
-
-
     }
 }
