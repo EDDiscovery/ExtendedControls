8cb6bd34ddaf657138c20668185948187a06a7eb
robbyxp1 <robxp@hotmail.com>
2022-02-03 13:45:04 +0000

    Move to using QuickJSON

--- ExtendedControls/Themes/Theme.cs
+++ ExtendedControls/Themes/Theme.cs
@@ -14,7 +14,7 @@
  * EDDiscovery is not affiliated with Frontier Developments plc.
  */
 
-using BaseUtils.JSON;
+using QuickJSON;
 using System;
 using System.Collections.Generic;
 using System.Drawing;
