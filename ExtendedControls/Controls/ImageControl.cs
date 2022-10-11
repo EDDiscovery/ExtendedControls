4c44a8e4d836d3ecd247f523edc88af0fe0c63a7
robbyxp1 <robxp@hotmail.com>
2022-10-11 16:24:42 +0100

    Add more image planes to ImageControl, allow control of visibility

--- ExtendedControls/Controls/ImageControl.cs
+++ ExtendedControls/Controls/ImageControl.cs
@@ -21,11 +21,15 @@ namespace ExtendedControls.Controls
 {
     public partial class ImageControl : Control
     {
-        public Size ImageSize { get; set; } = new Size(128, 128);
+        public Size ImageSize { get; set; } = new Size(128, 128);               // changing this wipes the bitmap at the next draw
+        public int ImageDepth { get; set; } = 1;                               // how many bitmaps to overlay
+        public bool[] ImageVisible { get; set; } = new bool[1];
+        public int ImageWidth { get { return ImageSize.Width; } }
+        public int ImageHeight { get { return ImageSize.Height; } }
         public ImageLayout ImageLayout { get; set; } = ImageLayout.Stretch;     // only stretch/none at the moment
         public Color ImageBackgroundColor { get; set; } = Color.Transparent;
 
-        private Bitmap bitmap = null;
+        private Bitmap[] bitmaps = null;
 
         public ImageControl()
         {
@@ -37,21 +41,21 @@ namespace ExtendedControls.Controls
         // Draw Text at pos. backcolor = null means transparent
 
         public void DrawText(Rectangle area, string text, Font dp, Color forecolor, Color? backcolor = null,
-                                                    float backscale = 1.0F, StringFormat frmt = null, int angleback = 90)
+                                                    float backscale = 1.0F, StringFormat frmt = null, int angleback = 90, int bitmap = 0)
         {
             CheckBitmap();
-            BaseUtils.BitMapHelpers.DrawTextIntoBitmap(bitmap, area, text, dp, forecolor, backcolor, backscale, frmt, angleback);
+            BaseUtils.BitMapHelpers.DrawTextIntoBitmap(bitmaps[bitmap], area, text, dp, forecolor, backcolor, backscale, frmt, angleback);
         }
         public void DrawText(Point pos, Size maxsize, string text, Font dp, Color forecolor, Color? backcolor = null,
-                                                    float backscale = 1.0F, StringFormat frmt = null, int angleback = 90)
+                                                    float backscale = 1.0F, StringFormat frmt = null, int angleback = 90, int bitmap = 0)
         {
             CheckBitmap();
-            BaseUtils.BitMapHelpers.DrawTextIntoBitmap(bitmap, pos, maxsize, text, dp, forecolor, backcolor, backscale, frmt, angleback);
+            BaseUtils.BitMapHelpers.DrawTextIntoBitmap(bitmaps[bitmap], pos, maxsize, text, dp, forecolor, backcolor, backscale, frmt, angleback);
         }
 
         // draw lines. The area determines the part to draw the backcolour on, and to size any centre formatting against
         public void DrawText(Rectangle area, string[] text, Font dp, int linespacing, Color forecolor, Color? backcolor = null,
-                                                    float backscale = 1.0F, StringFormat frmt = null, int angleback = 90)
+                                                    float backscale = 1.0F, StringFormat frmt = null, int angleback = 90, int bitmap = 0)
         {
             CheckBitmap();
 
@@ -61,7 +65,7 @@ namespace ExtendedControls.Controls
             {
                 if (x != null)
                 {
-                    BaseUtils.BitMapHelpers.DrawTextIntoBitmap(bitmap, area, x, dp, forecolor, firstforback ? backcolor: null, backscale, frmt, angleback);
+                    BaseUtils.BitMapHelpers.DrawTextIntoBitmap(bitmaps[bitmap], area, x, dp, forecolor, firstforback ? backcolor: null, backscale, frmt, angleback);
                     area.Y += dp.Height + linespacing;
                     firstforback = false;
                 }
@@ -70,7 +74,7 @@ namespace ExtendedControls.Controls
 
         // draw lines. Each area is individually back coloured
         public void DrawText(Point pos, Size maxsize, string[] text, Font dp, int linespacing, Color forecolor, Color? backcolor = null,
-                                                    float backscale = 1.0F, StringFormat frmt = null, int angleback = 90)
+                                                    float backscale = 1.0F, StringFormat frmt = null, int angleback = 90, int bitmap = 0)
         {
             CheckBitmap();
 
@@ -78,45 +82,69 @@ namespace ExtendedControls.Controls
             {
                 if (x != null)
                 {
-                    BaseUtils.BitMapHelpers.DrawTextIntoBitmap(bitmap, pos, maxsize, x, dp, forecolor, backcolor, backscale, frmt, angleback);
+                    BaseUtils.BitMapHelpers.DrawTextIntoBitmap(bitmaps[bitmap], pos, maxsize, x, dp, forecolor, backcolor, backscale, frmt, angleback);
                     pos.Y += dp.Height + linespacing;
                 }
             }
         }
 
-        public void DrawImage(Image img, Rectangle area)
+        public void DrawImage(Image img, Rectangle area, int bitmap = 0)
         {
             CheckBitmap();
-            using (Graphics gr = Graphics.FromImage(bitmap))
+            using (Graphics gr = Graphics.FromImage(bitmaps[bitmap]))
                 gr.DrawImage(img, area);
         }
 
         // remember to use Using to release the graphics
-        public Graphics GetGraphics()     
+        public Graphics GetGraphics(int bitmap = 0)     
         {
             CheckBitmap();
-            return Graphics.FromImage(bitmap);      
+            return Graphics.FromImage(bitmaps[bitmap]);      
         }
 
-        public void Clear()
+        public void Clear(int level = -1)
         {
-            if (bitmap != null)
+            if (bitmaps != null)
             {
-                using (Graphics gr = Graphics.FromImage(bitmap))
-                    gr.Clear(ImageBackgroundColor);
+                if (level == -1)
+                {
+                    foreach (var b in bitmaps)
+                    {
+                        using (Graphics gr = Graphics.FromImage(b))
+                            gr.Clear(ImageBackgroundColor);
+                    }
+                }
+                else
+                {
+                    using (Graphics gr = Graphics.FromImage(bitmaps[level]))
+                        gr.Clear(ImageBackgroundColor);
+                }
             }
         }
 
         private void CheckBitmap()
         {
-            if (bitmap == null)
+            if (bitmaps == null || bitmaps[0].Size != ImageSize || bitmaps.Length != ImageDepth)
             {
-                bitmap = new Bitmap(ImageSize.Width, ImageSize.Height);
-                if (ImageBackgroundColor != Color.Transparent)
+                if (bitmaps != null)
                 {
-                    using (Graphics gr = Graphics.FromImage(bitmap))
-                        gr.Clear(ImageBackgroundColor);
+                    foreach (var b in bitmaps)
+                        b?.Dispose();      // previous one, gone
+
+                    bitmaps = null;
                 }
+
+                ImageVisible = new bool[ImageDepth];
+
+                bitmaps = new Bitmap[ImageDepth];
+                for (int i = 0; i < bitmaps.Length; i++)
+                {
+                    bitmaps[i] = new Bitmap(ImageSize.Width, ImageSize.Height);
+                    ImageVisible[i] = true;
+                }
+
+                if (ImageBackgroundColor != Color.Transparent)
+                    Clear();
             }
         }
 
@@ -152,15 +180,23 @@ namespace ExtendedControls.Controls
 
         protected override void OnPaint(PaintEventArgs pe)
         {
-            if ( bitmap != null )
+            if ( bitmaps != null )
             {
                 if (ImageLayout == ImageLayout.Stretch)
                 {
-                    pe.Graphics.DrawImage(bitmap, ClientRectangle);
+                    for (int i = 0; i < bitmaps.Length; i++)
+                    {
+                        if (ImageVisible[i])
+                            pe.Graphics.DrawImage(bitmaps[i], ClientRectangle);
+                    }
                 }
                 else
                 {
-                    pe.Graphics.DrawImage(bitmap, new Point(0, 0));
+                    for (int i = 0; i < bitmaps.Length; i++)
+                    {
+                        if (ImageVisible[i])
+                            pe.Graphics.DrawImage(bitmaps[i], new Point(0, 0));
+                    }
                 }
             }
         }
